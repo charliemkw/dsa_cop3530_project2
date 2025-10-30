45fd43eb9fbb23a9465e46eea7a533a6608db01e
veronicaoi <vigwe05@gmail.com>
2025-10-29 23:37:06 -0400

    Updated Location class and added function to parse CSV

--- src/location.h
+++ src/location.h
@@ -5,24 +5,27 @@
 using namespace std;
 
 class Location {
-    string date; //A; If unknown, this is "0".
-    string owned; //B; OWNED or LEASED
-    int parking_spaces; //C
-    string active; //D; ACTIVE or INACTIVE
-    string building_type; //E
-    int congressional_district; //F
-    string location_id; //G
-    int location_region_id; //H; not sure what this represents?
-    string accessible; //I; "Will Conform" or "Yes" or "No"
-    int ansi; //J
-    string city; //K
-    string county; //L
-    string address; //M
-    string state; //N
-    string original_zip; //O; DO NOT NEED THIS 
-    string zipcode; //P; zip code with proper length
-    float latitude; //Q
-    float longitude; //R
+
+    private: //Making information private so that they cannot be changed
+        string date; //A; If unknown, this is "0".
+        string owned; //B; OWNED or LEASED
+        //^ making this a str so its easier to parse through csv but can change to bool if anyone wants :) 
+        int parking_spaces; //C
+        string active; //D; ACTIVE or INACTIVE
+        string building_type; //E
+        int congressional_district; //F
+        string location_id; //G
+        int location_region_id; //H; not sure what this represents?
+        string accessible; //I; "Will Conform" or "Yes" or "No"
+        int ansi; //J
+        string city; //K
+        string county; //L
+        string address; //M
+        string state; //N
+        string original_zip; //O; DO NOT NEED THIS 
+        string zipcode; //P; zip code with proper length; use this
+        float latitude; //Q
+        float longitude; //R
 
 
     Location(string a, string b, int c, string d, string e, int f, string g, int h, string i, int j, 
@@ -36,4 +39,42 @@ class Location {
             congressional_district = f;
             
         }
+
+    //Getters
+    string get_date(); //A
+
+    bool is_owned(); //B
+
+    int get_parking_spaces(); //C
+
+    bool is_active(); //D
+
+    string get_building_type(); //E
+
+    int get_district(); //F; congressional district
+
+    string get_id(); //G; location id
+
+    int get_region_id(); //H; location region ID
+
+    string get_accessibility(); //I
+
+    int get_ansi(); //J
+
+    string get_city(); //K
+
+    string get_county(); //L
+
+    string get_address(); //M
+
+    string get_state(); //N
+
+    string get_original_zip(); //O; May delete this as well since it likely won't be needed
+
+    string get_zip(); //P; This is our corrected zip code
+
+    float get_latitude(); //Q
+
+    float get_longitude(); //R
+
 };
