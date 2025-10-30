082e63b84fcc7adb64771a5c0ffd16db0ef44ff0
veronicaoi <vigwe05@gmail.com>
2025-10-29 23:37:06 -0400

    Updated Location class and added function to parse CSV

--- src/MinHeap.h
+++ src/MinHeap.h
@@ -2,26 +2,26 @@
 
 #include <vector>
 #include <iostream>
+#include "Location.h"
 using namespace std;
 
-struct Item {
-    double distance;
-};
+
 
 class MinHeap {
 private:
-    vector<Item> heap;
+    vector<Location> heap;
     int parent(int i) {return(i-1)/2;}
     int leftChild(int i){ return 2* i +1; }
     int rightChild(int i){ return 2*i+2;}
     void heapifyUp(int index);
     void heapifyDown(int index);
 
+
 public:
-    void insert(Item item);
-    Item extractMin();
-    void buildHeap( vector<Item>& items);
-    vector<Item> getTopK(int k);
+    void insert(Location loc);
+    Location extractMin();
+    void buildHeap( vector<Location>& locs);
+    vector<Location> getTopK(int k);
     int size() const {return heap.size(); }
     bool isEmpty() const {return heap.empty();}
     void printHeap();
--- src/location.cpp
+++ src/location.cpp
@@ -2,3 +2,80 @@
 #include "location.h"
 
 
+string Location::get_date(){
+    return this->date;
+}
+
+bool Location::is_owned(){
+    if (this->owned == "OWNED"){
+        return true;
+    }
+    return false;
+}
+
+int Location::get_parking_spaces(){
+    return this->parking_spaces;
+}
+
+bool Location::is_active(){
+    if (this->active == "ACTIVE"){
+        return true;
+    }
+    return false;
+}
+
+string Location::get_building_type(){
+    return this->building_type;
+}
+
+int Location::get_district(){
+    return this->congressional_district;
+}
+
+string Location::get_id(){
+    return this->location_id;
+}
+
+int Location::get_region_id(){
+    return this->location_region_id;
+}
+
+string Location::get_accessibility(){
+    return this->accessible;
+}
+
+int Location::get_ansi(){
+    return this->ansi;
+}
+
+string Location::get_city(){
+    return this->city;
+}
+
+string Location::get_county(){
+    return this->county;
+}
+
+string Location::get_address(){
+    return this->address;
+}
+
+string Location::get_state(){
+    return this->state;
+}
+
+string Location::get_original_zip(){
+    return this->original_zip;
+}
+
+string Location::get_zip(){
+    return this->zipcode;
+}
+
+float Location::get_latitude(){
+    return this->latitude;
+}
+
+float Location::get_longitude(){
+    return this->longitude;
+}
